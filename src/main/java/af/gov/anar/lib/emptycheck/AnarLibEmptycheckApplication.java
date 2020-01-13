d2815b9fce15e7e34ed7a36cb768c18baf4d551c
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 13:36:01 +0430

    empty check utility library

---
+++ src/main/java/af/gov/anar/lib/emptycheck/AnarLibEmptycheckApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.emptycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibEmptycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibEmptycheckApplication.class, args);
+	}
+
+}
